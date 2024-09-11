9281f3980056bfb2bf775e7bf27fe89e459eac1c
hanwj75 <wjws9608@gmail.com>
2024-09-11 15:48:48 +0900

    auth.middleware Update
    토큰 검즘기능 수정

---
+++ middlewaers/auth.middleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import { prisma } from "../utils/prisma/index.js";
+
+const env = process.env;
+
+export default async (req, res, next) => {
+  try {
+    const authHeader = req.headers["authorization"];
+
+    if (!authHeader) {
+      throw new Error("요청한 사용자의 토큰이 존재하지 않습니다.");
+    }
+    const [tokenType, JWT_ACCESS_TOKEN] = authHeader.split(" ");
+    if (tokenType !== "Bearer")
+      throw new Error("토큰 타입이 Bearer가 아닙니다.");
+    const decodedToken = jwt.verify(JWT_ACCESS_TOKEN, env.JWT_TOKEN_SECRETKEY);
+    const userId = decodedToken.userId;
+
+    const user = await prisma.account.findUnique({
+      where: { userId: userId },
+    });
+    if (!user) {
+      throw new Error("토큰 사용자가 존재하지 않습니다.");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: error.message });
+  }
+};
