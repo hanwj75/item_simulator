e6e39db72bb68c5a10bce1d3b0efcd5c4f7fa049
hanwj75 <wjws9608@gmail.com>
2024-09-12 11:14:27 +0900

    Update error
    에러 출력 status code 변경

--- routes/users.router.js
+++ routes/users.router.js
@@ -10,82 +10,88 @@ const router = express.Router();
 
 router.post("/sign-up", async (req, res, next) => {
   const { userId, userPassword, checkPassword, userName } = req.body;
+  try {
+    //비밀번호 일치여부 확인
+    if (
+      !userPassword ||
+      !checkPassword ||
+      userPassword !== checkPassword ||
+      userPassword.length < 6
+    ) {
+      return res.status(400).json({ message: "비밀번호가 일치하지 않습니다." });
+    }
 
-  //비밀번호 일치여부 확인
-  if (
-    !userPassword ||
-    !checkPassword ||
-    userPassword !== checkPassword ||
-    userPassword.length < 6
-  ) {
-    return res.status(400).json({ message: "비밀번호가 일치하지 않습니다." });
+    //사용자 존재여부 확인
+    const isExistUser = await prisma.account.findUnique({
+      where: {
+        userId,
+      },
+    });
+
+    if (isExistUser) {
+      return res.status(409).json({ message: "이미 존재하는 아이디입니다." });
+    }
+    //비밀번호 해시화
+    const hashedPassword = await bcrypt.hash(userPassword, 10);
+
+    // Users 테이블에 사용자를 추가합니다.
+    const user = await prisma.account.create({
+      data: {
+        userId,
+        userPassword: hashedPassword,
+        userName,
+      },
+    });
+
+    return res.status(201).json({
+      message: "회원가입이 완료되었습니다.",
+      accountId: user.accountId,
+      userId: user.userId,
+      userName: user.userName,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "서버 오류가 발생하였습니다." });
   }
-
-  //사용자 존재여부 확인
-  const isExistUser = await prisma.account.findUnique({
-    where: {
-      userId,
-    },
-  });
-
-  if (isExistUser) {
-    return res.status(409).json({ message: "이미 존재하는 아이디입니다." });
-  }
-  //비밀번호 해시화
-  const hashedPassword = await bcrypt.hash(userPassword, 10);
-
-  // Users 테이블에 사용자를 추가합니다.
-  const user = await prisma.account.create({
-    data: {
-      userId,
-      userPassword: hashedPassword,
-      userName,
-    },
-  });
-
-  return res.status(201).json({
-    message: "회원가입이 완료되었습니다.",
-    accountId: user.accountId,
-    userId: user.userId,
-    userName: user.userName,
-  });
 });
 
 /*사용자 로그인 페이지*/
 
 router.post("/login", async (req, res, next) => {
   const { userId, userPassword } = req.body;
+  try {
+    //사용자 아이디 확인
+    const user = await prisma.account.findUnique({
+      where: {
+        userId: userId,
+      },
+    });
+    //사용자 존재 여부 확인
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "아이디 또는 비밀번호가 일치하지 않습니다." });
+    }
+    //비밀번호 확인
+    const passwordValid = await bcrypt.compare(userPassword, user.userPassword);
+    if (!passwordValid) {
+      return res
+        .status(401)
+        .json({ message: "아이디 또는 비밀번호가 일치하지 않습니다." });
+    }
+    //jwt 토큰 생성
+    const JWT_ACCESS_TOKEN = jwt.sign(
+      { userId: user.userId },
+      env.JWT_TOKEN_SECRETKEY,
+      { expiresIn: "1h" },
+    );
+    res.header("authorization", `Bearer ${JWT_ACCESS_TOKEN}`);
 
-  //사용자 아이디 확인
-  const user = await prisma.account.findUnique({
-    where: {
-      userId: userId,
-    },
-  });
-  //사용자 존재 여부 확인
-  if (!user) {
-    return res
-      .status(401)
-      .json({ message: "아이디 또는 비밀번호가 일치하지 않습니다." });
+    return res.status(200).json({
+      message: "로그인에 성공했습니다.",
+      JWT_ACCESS_TOKEN: JWT_ACCESS_TOKEN,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "서버 오류가 발생하였습니다." });
   }
-  //비밀번호 확인
-  const passwordValid = await bcrypt.compare(userPassword, user.userPassword);
-  if (!passwordValid) {
-    return res
-      .status(401)
-      .json({ message: "아이디 또는 비밀번호가 일치하지 않습니다." });
-  }
-  //jwt 토큰 생성
-  const JWT_ACCESS_TOKEN = jwt.sign(
-    { userId: user.userId },
-    env.JWT_TOKEN_SECRETKEY,
-    { expiresIn: "1h" },
-  );
-  res.header("authorization", `Bearer ${JWT_ACCESS_TOKEN}`);
-
-  return res.status(200).json({
-    message: "로그인에 성공했습니다.",
-    JWT_ACCESS_TOKEN: JWT_ACCESS_TOKEN,
-  });
 });
 export default router;
